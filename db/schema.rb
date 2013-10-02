085f341010e4bb515978d585ae0c4513bf82e3b2
Justin Faulkner <justinfaulkner@gmail.com>
2013-10-01 21:38:42 -0700

    Adding a password for the the_wordler

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131002034001) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "daily_counts", force: true do |t|
     t.integer  "count"
     t.datetime "day"
