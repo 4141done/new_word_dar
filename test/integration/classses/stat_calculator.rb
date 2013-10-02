b0b4d7c5d71f5d10d853f7d29ccbcd5579f6ea7e
Justin Faulkner <justinfaulkner@gmail.com>
2013-10-01 23:00:49 -0700

    adding test cases for stat calc

--- test/integration/classses/stat_calculator.rb
+++ test/integration/classses/stat_calculator.rb
@@ -7,18 +7,27 @@ describe StatCalculator do
     DailyCount.create(count: 3, day: Time.now-1.day)
     DailyCount.create(count: 2, day: Time.now-2.days)
     DailyCount.create(count: 1, day: Time.now-3.days)
+
+    @calc = StatCalculator.new(word)
   end
 
   it "should show today's word counts" do
-    
+    @calc.today.must_equal 4
   end
 
   it "should show yesterday's word counts" do
-
+    @calc.yesterday.must_equal 3
   end
 
   it "should show this week's word counts" do
-
+    today = Time.now.date
+    @calc.this_week[today].must_equal 4
+    @calc.this_week[today-1.day].must_equal 3
+    @calc.this_week[today-2.days].must_equal 2
+    @calc.this_week[today-3.days].must_equal 1
+    @calc.this_week[today-4.days].must_equal 0
+    @calc.this_week[today-5.days].must_equal 0
+    @calc.this_week[today-6.days].must_equal 0
   end
 
   it "should show last week's word counts" do
