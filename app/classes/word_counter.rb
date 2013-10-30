d3a669963fd0b7bd4917c3fa0dde9635cd7ff209
Joseph Chen <joseph@urbanspoon.com>
2013-10-29 22:36:38 -0700

    Adding context to word counting

--- app/classes/word_counter.rb
+++ app/classes/word_counter.rb
@@ -4,19 +4,18 @@ class WordCounter
   # d = File.open('lib/2of12inf.txt').lines.map { |word| word.strip.downcase }
   def initialize blacklist=[]
     @blacklist = Set.new blacklist
+    @contexts = {}
   end
 
-  # TODO: context?, try to save the sentence for context
-  def process string, context=nil, date=Time.now
-    words = clean_and_tokenize string
-    words_to_count = filter_words words
+  def process string, date=Time.now
+    words_with_context = clean_and_tokenize_with_context string
+    words_to_count = filter_words words_with_context
     counts = count_words words_to_count
 
-    counts.each { |word, count|
+    counts.each { |word_with_context, count|
+      word = word_with_context[0]
+      context = word_with_context[1]
       word_record = Word.find_by_word word
-      #if word_record.blank?
-      #  word_record = create_word_record word, string
-      #end
       word_record ||= Word.create! word: word, context: context
       count_record = word_record.daily_counts.where(day: date.to_date).first
       count_record ||= DailyCount.create! count: 0, day: date.to_date, word: word_record
@@ -26,31 +25,32 @@ class WordCounter
   end
 
 
-  def clean_and_tokenize string
-    s = string.gsub(/\n/, ' ').gsub(/ /, '_').gsub(/_/, ' ').downcase
-    words = s.split(' ').compact
-    long_words = words.select { |w| (w.length > 3 && w.length < 255) }
-    return long_words
+  def clean_and_tokenize_with_context string
+    words = []
+    sentences = string.split(/[.!?] |\n/)
+    sentences.each { |sentence|
+      s = sentence.gsub(/\n/, ' ').gsub(/ /, '_').gsub(/_/, ' ').downcase
+      sentence_words = s.split(' ').compact
+      sentence_words = sentence_words.select { |w| (w.length > 3 && w.length < 255) }
+      words = words + sentence_words.map { |sw| [sw, sentence[0...255]] }
+    }
+    return words
   end
 
-  def filter_words words
-    filtered_words = words.select { |w| !@blacklist.include?(w) && !w.match(/\W/) }
+  def filter_words words_with_context
+    filtered_words = words_with_context.select { |w| !@blacklist.include?(w[0]) && !w[0].match(/\W/) }
     return filtered_words
   end
 
-  def count_words words
+  def count_words words_with_context
     counts = {}
 
-    words.each { |word|
-      counts[word] ||= 0
-      counts[word] += 1
+    words_with_context.each { |word_with_context|
+      counts[word_with_context] ||= 0
+      counts[word_with_context] += 1
     }
 
     return counts
-  end
-
-  def create_word_record word, string
-
   end
 
 end
